c6c1e84dfa0259132097995f8782d9984c980434
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2025-10-01 20:18:42 +0630

    Add project files.

---
+++ TechnortalDotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -0,0 +1,125 @@
+﻿using TechnortalDotNetTrainingBatch3.ConsoleApp;
+
+//Student student = new Student(1, "mg mg", new DateTime(2000, 1, 1));
+////student.StudentNo = 1;
+////student.StudentName = "John Doe";   
+
+//Student student2 = new Student(2, "zaw zaw", new DateTime(2000, 1, 1));
+
+//Student student3 = new Student();
+////student2.StudentNo = 2;
+////student2.StudentName = "John Doe 2";
+////student2.DateOfBirth = new DateTime(2000, 1, 1);
+
+
+//Console.WriteLine(student.StudentName);
+//Console.WriteLine(student2.StudentName);
+
+//Wallet wallet = new Wallet();
+//wallet.Name = "Mg Mg";
+//wallet.MobileNo = "09123456789";
+//wallet.amount = 1000;
+////wallet.Balance = 5000;
+//wallet.SetBalance(30000);
+////wallet.ShowBalance();
+//Animal animal = new Animal();
+
+//EnumUserType userType = EnumUserType.Guest;
+//switch (userType)
+//{
+//    case EnumUserType.Guest:
+//        break;
+
+//    case EnumUserType.User:
+//        break;
+
+//    case EnumUserType.Admin:
+//        break;
+   
+//    case EnumUserType.SuperAdmin:
+//        break;
+
+//    case EnumUserType.None:
+//    default:
+//        break;
+//}
+
+//string userType2 = string.Empty;
+//switch (userType2)
+//{
+//    default:
+//        break;
+//}
+
+string writeText = "Hello World!";  // Create a text string
+File.WriteAllText("collin.txt", writeText);  // Create a file and write the content of writeText to it
+
+
+Console.Read();
+
+public abstract class Animal
+{
+    public abstract void animalSound();
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void animalSound()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface ITransfer
+{
+    void Transfer(string fromMobileNo, string toMobileNo, decimal amount);
+    void TransactionHistory(string mobileNo);
+}
+
+public class Wallet : ITransfer
+{
+    public void TransactionHistory(string mobileNo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Transfer(string fromMobileNo, string toMobileNo, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Bank : ITransfer
+{
+    //public void TransactionHistory(string mobileNo)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    //public void Transfer(string fromMobileNo, string toMobileNo, decimal amount)
+    //{
+    //    throw new NotImplementedException();
+    //}
+    public void TransactionHistory(string mobileNo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Transfer(string fromMobileNo, string toMobileNo, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum EnumUserType
+{
+    None,
+    Guest,
+    User,
+    Admin,
+    SuperAdmin
+}
---
+++ TechnortalDotNetTrainingBatch3.ConsoleApp/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnortalDotNetTrainingBatch3.ConsoleApp
+{
+    // public / private / internal / protected
+    internal class Student : Father
+    {
+        public Student()
+        {
+            StudentName = "--Test--";
+        }
+
+        public Student(int studentNo, string studentName, DateTime dateOfBirth)
+        {
+            StudentName = "--Test--";
+        }
+
+        // Properties
+        public int StudentNo { get; set; }
+        public string StudentName { get; set; } 
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class Father
+    {
+        protected string ProName { get; set; }
+
+        protected void Test()
+        {
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Name;
+        public int EmployeeNo { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class Wallet
+    {
+        public decimal amount; // fields
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+        public decimal Balance { get; private set; } // properties
+        public decimal ShowBalance()
+        {
+            return Balance;
+        }
+        public void SetBalance(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new Exception("Amount must be greater than 0");
+            }
+            Balance = amount;
+        }
+    }
+}
